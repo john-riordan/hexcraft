dbe1261c2badd51d26ac790da91df78d628fb659
John Riordan <iam.john.riordan@gmail.com>
2021-06-15 22:00:45 -0700

    Use next 11 and fix next image layout issue

--- Components/ItemImage/index.js
+++ Components/ItemImage/index.js
@@ -4,23 +4,25 @@ import styles from './Image.module.css';
 
 const ItemImage = ({
   imgName,
-  alt = 'Season 2021 Item',
   size = 64,
   className,
   isMythic = false,
   inline = false,
 }) => (
   <div
-    className={`${styles.imgFrame} ${inline && styles.inline}  ${className}`}
+    className={`${styles.imgFrame} ${
+      inline && styles.inline
+    }  ${className}`}
   >
-    {isMythic && <div className={styles.mythicBorder} />}
+    {isMythic && <span className={styles.mythicBorder} />}
     <Image
       src={`/${imgName}`}
       width={size}
       height={size}
       quality={80}
-      alt={alt}
-      loading="lazy"
+      alt={imgName}
+      placeholder='blur'
+      blurDataURL='data:image/jpeg;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAIAAoDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAb/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWEREiMxUf/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAGhEAAgIDAAAAAAAAAAAAAAAAAAECEgMRkf/aAAwDAQACEQMRAD8AltJagyeH0AthI5xdrLcNM91BF5pX2HaH9bcfaSXWGaRmknyJckliyjqTzSlT54b6bk+h0R//2Q=='
     />
   </div>
 );
