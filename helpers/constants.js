d039fea6fa4a789d8d9752651b73b74dcdc1ae3a
MARCEL <16888873+1337MARCEL@users.noreply.github.com>
2021-07-08 17:04:30 +0200

    fix: add workaround to hide non SR items

--- helpers/constants.js
+++ helpers/constants.js
@@ -5,3 +5,11 @@ export const CHANGE_TYPES = {
   nerf: 'Nerf',
   change: 'Change',
 };
+
+export const BLACKLISTED_ITEMS = {
+  2051: "Guardian's Horn",
+  3112: "Guardian's Orb",
+  3177: "Guardian's Blad",
+  3184: "Guardian's Hammer",
+  4403: "The Golden Spatula",
+}
