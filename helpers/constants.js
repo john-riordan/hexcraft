1f8d8a9b42e2fe1afc930ca0c1b032be6f7f1d2d
John Riordan <iam.john.riordan@gmail.com>
2023-05-17 20:53:57 -0700

    fix: equinox flag

--- helpers/constants.js
+++ helpers/constants.js
@@ -14,7 +14,6 @@ export const BLACKLISTED_ITEMS = {
   3177: "Guardian's Blad",
   3184: "Guardian's Hammer",
   4403: 'The Golden Spatula',
-  7023: 'Equinox', // Rito didnt flag this as an Ornn item
 };
 
 export const MYTHIC_WHITELIST = {
