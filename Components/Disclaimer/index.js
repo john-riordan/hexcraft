bf9ecbfbc7f8d1aaa65ea49867970d71c3b539b4
John Riordan <iam.john.riordan@gmail.com>
2023-11-20 16:16:25 -0800

    feat: disclaimer text

--- Components/Disclaimer/index.js
+++ Components/Disclaimer/index.js
@@ -4,6 +4,7 @@ const Disclaimer = () => {
   return (
     <div className={styles.disclaimer}>
       <h2>Season 2024 items and data may be incomplete while on PBE</h2>
+      <span>Categories will be updated shortly</span>
     </div>
   );
 };
