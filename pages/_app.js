e65924bbda40ea9451291836d520312dbb9205cc
John Riordan <iam.john.riordan@gmail.com>
2025-01-23 01:04:03 -0800

    chore: vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css';
+import { Analytics } from "@vercel/analytics/next";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
