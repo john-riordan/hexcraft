6a3bd5d84b0ce64b43f30148619aefdeb65ec72e
John Riordan <iam.john.riordan@gmail.com>
2025-05-05 02:42:20 -0700

    feat: patch 15.9

--- data/patches.js
+++ data/patches.js
@@ -1,68 +1,140 @@
 import { CHANGE_TYPES } from "../helpers/constants";
 
 export const PATCHES = {
-  "15.8.1": {
-    3137: {
-      change: CHANGE_TYPES.buff,
+  "15.9.1": {
+    3009: {
+      change: CHANGE_TYPES.nerf,
       details: `
         <div>
-					<h4 class="change-detail-title ability-title"><img src="https://am-a.akamaihd.net/image?f=https://ddragon.leagueoflegends.com/cdn/15.7.1/img/item/3137.png">Cryptbloom</h4>
+					<a href="https://www.leagueoflegends.com/en-us/how-to-play/" class="reference-link"><img src="https://am-a.akamaihd.net/image?f=https://ddragon.leagueoflegends.com/cdn/15.8.1/img/item/3009.png"></a>
+					<h3 id="patch-Boots-of-Swiftness" class="change-title"><a href="https://www.leagueoflegends.com/en-us/how-to-play/">Boots of Swiftness</a></h3>
 					<blockquote class="blockquote context">
-						Cryptbloom has fallen completely behind Void Staff for mages and we'd like this comparison to be fairer. As a result, we're delivering a light buff to Cryptbloom that should help to keep it distinct based on what you're looking for.
+						Boots of Swiftness are a bit overpowered relative to other boots, leading to champions zipping around combat more than we’d like. Ultimately, we’re going with a small direct nerf that should keep it in line with other boot options.
+						
 					</blockquote>
-					<hr class="divider">
 					<ul>
-						<li><strong>Ability Haste</strong>: 15 ⇒ <strong>20</strong></li>
+						<li><strong>Movement Speed</strong>: 60 ⇒ 55</li>
 					</ul>
 				</div>
       `,
     },
-    3742: {
+    3010: {
       change: CHANGE_TYPES.nerf,
       details: `
-				<div>
-					<h4 class="change-detail-title ability-title"><img src="https://am-a.akamaihd.net/image?f=https://ddragon.leagueoflegends.com/cdn/15.7.1/img/item/3742.png">Dead Man's Plate</h4>
+        <div>
+					<a href="https://www.leagueoflegends.com/en-us/how-to-play/" class="reference-link"><img src="https://am-a.akamaihd.net/image?f=https://ddragon.leagueoflegends.com/cdn/15.8.1/img/item/3010.png"></a>
+					<h3 id="patch-Symbiotic-Soles" class="change-title"><a href="https://www.leagueoflegends.com/en-us/how-to-play/">Symbiotic Soles</a></h3>
 					<blockquote class="blockquote context">
-						The slow resistance stat on Dead Man's Plate has been too effective at covering a few champion's core weakness lately and we feel like this is a good time to reduce the potency of the slow resist on a single purchase.
+						Symbiotic Soles are a solid option for supports, which we’re very happy about, but they’re too game-warping for laners, as the empowered Recall lets them quickly reset every few waves, removing all sense of attrition. So we’re making some adjustments to retain their power for roaming around the map but make them worse when spamming Recall over and over.
+						
 					</blockquote>
-					<hr class="divider">
 					<ul>
-						<li><strong>Slow Resist</strong>: 25% ⇒ <strong>15%</strong></li>
+						<li><strong>NEW</strong>: Now grants 10 out of combat Movement Speed</li>
+						<li><strong>REMOVED</strong>: No longer grants Empowered Recall</li>
 					</ul>
 				</div>
-			`,
+      `,
     },
-    6701: {
+    6657: {
+      change: CHANGE_TYPES.nerf,
+      details: `
+        <div>
+					<a href="https://www.leagueoflegends.com/en-us/how-to-play/" class="reference-link"><img src="https://am-a.akamaihd.net/image?f=https://ddragon.leagueoflegends.com/cdn/15.8.1/img/item/6657.png"></a>
+					<h3 id="patch-Rod-of-Ages" class="change-title"><a href="https://www.leagueoflegends.com/en-us/how-to-play/">Rod of Ages</a></h3>
+					<blockquote class="blockquote context">
+						Rod of Ages’ new build path has made it overtake mage mana itemization, and as a result, its high durability has slowed down champion interactions below what we think makes for the best gameplay. We think as a whole, mages are rather balanced and we intend to ship a larger pass on mage itemization to make sure all AP items are worthy of purchasing in an upcoming patch, but right now we want to tap the newly-popular Rod of Ages to make sure bursty champions can attack more normal-sized health pools.
+						
+					</blockquote>
+					<ul>
+						<li><strong>Initial Health</strong>: 400 ⇒ 350</li>
+						<li><strong>Maximum Health</strong>: 500 ⇒ 450</li>
+						<li><strong>Initial Ability Power</strong>: 50 ⇒ 45</li>
+						<li><strong>Maximum Ability Power</strong>: 80 ⇒ 75</li>
+					</ul>
+				</div>
+      `,
+    },
+    3174: {
       change: CHANGE_TYPES.buff,
       details: `
-				<div>
-					<h4 class="change-detail-title ability-title"><img src="https://am-a.akamaihd.net/image?f=https://ddragon.leagueoflegends.com/cdn/15.7.1/img/item/6701.png">Opportunity</h4>
-					<blockquote class="blockquote context">
-						Opportunity has dropped in popularity over time and could use a buff to make it a more competitive option in the Lethality item space. Additionally we're lowering the passive Lethality gain on the ranged split so ranged users don't benefit quite as much from the base stat buffs as melee users do.
-					</blockquote>
-					<hr class="divider">
+        <div>
+					<h4 class="change-detail-title">Armored Advance</h4>
 					<ul>
-						<li><strong>Lethality</strong>: 15 ⇒ <strong>18</strong></li>
-						<li><strong>Preparation</strong>: After being out-of-combat with enemy champions for 8 seconds, gain 11 (melee) / 7 (ranged) lethality. ⇒ After being out-of-combat with enemy champions for 8 seconds, gain <strong>11 (melee) / 5 (ranged)</strong> lethality.</li>
+						<li><strong>Cost</strong>: 750 ⇒ 500 gold</li>
 					</ul>
 				</div>
-			`,
+      `,
     },
-    2501: {
+    3173: {
       change: CHANGE_TYPES.buff,
       details: `
-				<div>
-					<h4 class="change-detail-title ability-title"><img src="https://am-a.akamaihd.net/image?f=https://ddragon.leagueoflegends.com/cdn/15.7.1/img/item/2501.png">Overlord's Bloodmail</h4>
-					<blockquote class="blockquote context">
-						Overlord's has been fine, but not exceptionally powerful, for most of its lifetime. Looking to buff it into a position where it's clearly better than some other items for heavier fighters, especially in the later slots.
-					</blockquote>
-					<hr class="divider">
+        <div>
+					<h4 class="change-detail-title">Armored Advance</h4>
 					<ul>
-						<li><strong>Tyranny</strong>: Bonus AD equal to 2% of bonus health ⇒ <strong>Bonus AD equal to 2.5% of bonus health</strong></li>
-						<li><strong>Retribution</strong>: 0-10% (based on missing health) bonus AD ⇒ <strong>0-12% (based on missing health) bonus AD</strong></li>
+						<li><strong>Cost</strong>: 750 ⇒ 500 gold</li>
 					</ul>
 				</div>
-			`,
+      `,
+    },
+    3171: {
+      change: CHANGE_TYPES.buff,
+      details: `
+        <div>
+					<h4 class="change-detail-title">Armored Advance</h4>
+					<ul>
+						<li><strong>Cost</strong>: 750 ⇒ 500 gold</li>
+					</ul>
+				</div>
+      `,
+    },
+    3176: {
+      change: CHANGE_TYPES.buff,
+      details: `
+        <div>
+					<h4 class="change-detail-title">Forever Forward</h4>
+					<ul>
+						<li><strong>Cost</strong>: 750 ⇒ 500 gold</li>
+						<li><strong>Total Bonus Movespeed Out of Combat</strong>: 10% ⇒ 8%</li>
+					</ul>
+				</div>
+      `,
+    },
+    3172: {
+      change: CHANGE_TYPES.buff,
+      details: `
+        <div>
+					<h4 class="change-detail-title">Armored Advance</h4>
+					<ul>
+						<li><strong>Cost</strong>: 750 ⇒ 500 gold</li>
+					</ul>
+				</div>
+      `,
+    },
+    3175: {
+      change: CHANGE_TYPES.buff,
+      details: `
+        <div>
+					<h4 class="change-detail-title">Spellslinger’s Shoes</h4>
+					<ul>
+						<li><strong>Cost</strong>: 750 ⇒ 500 gold</li>
+						<li><strong>Magic Penetration</strong>: 8% ⇒ 7%</li>
+					</ul>
+				</div>
+      `,
+    },
+    3170: {
+      change: CHANGE_TYPES.buff,
+      details: `
+        <div>
+					<h4 class="change-detail-title">Swiftmarch</h4>
+					<ul>
+						<li><strong>Cost</strong>: 750 ⇒ 500 gold</li>
+						<li><strong>Movement Speed</strong>: 70 ⇒ 65</li>
+						<li><strong>REMOVED</strong>: No longer grants 4% increased total Movement Speed</li>
+						<li><strong>NEW</strong>: Now grants Adaptive Force equal to 5% of total Movement Speed</li>
+					</ul>
+				</div>
+      `,
     },
   },
 };
