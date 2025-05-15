13cd73ab14be0518a407fe3cde99a385c8ec3d66
John Riordan <iam.john.riordan@gmail.com>
2025-05-15 15:04:53 -0700

    feat: patch 15.10

--- data/patches.js
+++ data/patches.js
@@ -1,140 +1,170 @@
 import { CHANGE_TYPES } from "../helpers/constants";
 
 export const PATCHES = {
-  "15.9.1": {
-    3009: {
+  "15.10.1": {
+    4010: {
+      change: CHANGE_TYPES.buff,
+      details: `
+        <div>
+        		<h3 class="change-detail-title ability-title"><img src="https://am-a.akamaihd.net/image?f=https://ddragon.leagueoflegends.com/cdn/15.9.1/img/item/4010.png">Bloodletter's Curse</h3>
+					<ul>
+						<li><strong>Ability Power</strong>: 60 ⇒ <strong>65</strong></li>
+						<li><strong>Health</strong>: 350 ⇒ <strong>400</strong></li>
+						<li><strong>Magic Resist Shred per Stack</strong>: 5% ⇒ <strong>7.5%</strong></li>
+						<li><strong>Max Shred</strong>: 30% (unchanged)</li>
+					</ul>
+				</div>
+      `,
+    },
+    3137: {
+      change: CHANGE_TYPES.buff,
+      details: `
+				<div>
+        		<h3 class="change-detail-title ability-title"><img src="https://am-a.akamaihd.net/image?f=https://ddragon.leagueoflegends.com/cdn/15.9.1/img/item/3137.png">Cryptbloom</h3>
+					<ul>
+						<li><strong>Ability Power</strong>: 60 ⇒ <strong>75</strong></li>
+						<li><strong>Life from Death Heal</strong>: 100 (+ 25% AP) ⇒ 100 <strong>(+ 20% AP)</strong></li>
+						<li><strong>Recipe</strong>: Blighting Jewel + Fiendish Codex + 900 Gold ⇒ <strong>Blighting Jewel + Fiendish Codex + Fiendish Codex + 200 Gold</strong></li>
+						<li><strong>Total Gold Cost</strong>: 2850 ⇒ <strong>3000</strong></li>
+					</ul>
+				</div>
+			`,
+    },
+    3152: {
+      change: CHANGE_TYPES.buff,
+      details: `
+				<div>
+        		<h3 class="change-detail-title ability-title"><img src="https://am-a.akamaihd.net/image?f=https://ddragon.leagueoflegends.com/cdn/15.9.1/img/item/223152.png">Hextech Rocketbelt</h3>
+					<ul>
+						<li><strong>Ability Power</strong>: 60 ⇒ <strong>70</strong></li>
+						<li><strong>Health</strong>: 350 ⇒ <strong>300</strong></li>
+						<li><strong>Ability Haste</strong>: 15 ⇒ <strong>20</strong></li>
+						<li><strong>Recipe</strong>: Alternator + Kindlegem + 700 Gold ⇒ <strong>Alternator + Fiendish Codex + Ruby Crystal + 300 Gold</strong></li>
+						<li><strong>Total Gold Cost</strong>: 2600 ⇒ <strong>2650</strong></li>
+						<li>Updated cast indicator to more accurately represent bolt angles and ranges.</li>
+						<li>Item may now be manually cast with a click to confirm.</li>
+					</ul>
+				</div>
+			`,
+    },
+    4628: {
+      change: CHANGE_TYPES.buff,
+      details: `
+				<div>
+        		<h3 class="change-detail-title ability-title"><img src="https://am-a.akamaihd.net/image?f=https://ddragon.leagueoflegends.com/cdn/15.9.1/img/item/4628.png">Horizon Focus</h3>
+					<ul>
+						<li><strong>Ability Power</strong>: 75 ⇒ <strong>110</strong></li>
+						<li><strong>Hypershot Damage Amp</strong>: <strong>Removed</strong></li>
+						<li><strong>Recipe</strong>: Fiendish Codex + Fiendish Codex + Amplifying Tome + 600 Gold ⇒ <strong>Fiendish Codex + Fiendish Codex + Blasting Wand + 350 Gold</strong></li>
+						<li><strong>Total Gold Cost</strong>: 2700 ⇒ <strong>2900</strong></li>
+					</ul>
+				</div>
+			`,
+    },
+    6653: {
       change: CHANGE_TYPES.nerf,
       details: `
-        <div>
-					<a href="https://www.leagueoflegends.com/en-us/how-to-play/" class="reference-link"><img src="https://am-a.akamaihd.net/image?f=https://ddragon.leagueoflegends.com/cdn/15.8.1/img/item/3009.png"></a>
-					<h3 id="patch-Boots-of-Swiftness" class="change-title"><a href="https://www.leagueoflegends.com/en-us/how-to-play/">Boots of Swiftness</a></h3>
-					<blockquote class="blockquote context">
-						Boots of Swiftness are a bit overpowered relative to other boots, leading to champions zipping around combat more than we’d like. Ultimately, we’re going with a small direct nerf that should keep it in line with other boot options.
-						
-					</blockquote>
+				<div>
+        		<h3 class="change-detail-title ability-title"><img src="https://am-a.akamaihd.net/image?f=https://ddragon.leagueoflegends.com/cdn/15.9.1/img/item/6653.png">Liandry's Torment</h3>
 					<ul>
-						<li><strong>Movement Speed</strong>: 60 ⇒ 55</li>
+						<li><strong>Ability Power</strong>: 70 ⇒ <strong>60</strong></li>
 					</ul>
 				</div>
-      `,
+			`,
     },
-    3010: {
+    3100: {
+      change: CHANGE_TYPES.change,
+      details: `
+				<div>
+        		<h3 class="change-detail-title ability-title"><img src="https://am-a.akamaihd.net/image?f=https://ddragon.leagueoflegends.com/cdn/15.9.1/img/item/3100.png">Lich Bane</h3>
+					<ul>
+						<li><strong>Ability Power</strong>: 115 ⇒ <strong>100</strong></li>
+						<li><strong>Recipe</strong>: Sheen + Aether Wisp + Needlessly Large Rod + 200 Gold ⇒ <strong>Sheen + Aether Wisp + Blasting Wand + 250 Gold</strong></li>
+						<li><strong>Total Gold Cost</strong>: 3200 ⇒ <strong>2900</strong></li>
+					</ul>
+				</div>
+			`,
+    },
+    6655: {
+      change: CHANGE_TYPES.buff,
+      details: `
+				<div>
+        		<h3 class="change-detail-title ability-title"><img src="https://am-a.akamaihd.net/image?f=https://ddragon.leagueoflegends.com/cdn/15.9.1/img/item/6655.png">Luden's Companion</h3>
+					<ul>
+						<li><strong>Total Gold Cost</strong>: 2850 ⇒ <strong>2750</strong></li>
+						<li>Tooltip now shows the maximum single target damage.</li>
+					</ul>
+				</div>
+			`,
+    },
+    3118: {
+      change: CHANGE_TYPES.buff,
+      details: `
+				<div>
+        		<h3 class="change-detail-title ability-title"><img src="https://am-a.akamaihd.net/image?f=https://ddragon.leagueoflegends.com/cdn/15.9.1/img/item/3118.png">Malignance</h3>
+					<ul>
+						<li><strong>Ability Power</strong>: 85 ⇒ <strong>90</strong></li>
+					</ul>
+				</div>
+			`,
+    },
+    3165: {
+      change: CHANGE_TYPES.buff,
+      details: `
+				<div>
+        		<h3 class="change-detail-title ability-title"><img src="https://am-a.akamaihd.net/image?f=https://ddragon.leagueoflegends.com/cdn/15.9.1/img/item/3165.png">Morellonomicon</h3>
+					<ul>
+						<li><strong>Total Gold Cost</strong>: 2950 ⇒ <strong>2850</strong></li>
+					</ul>
+				</div>
+			`,
+    },
+    3115: {
+      change: CHANGE_TYPES.buff,
+      details: `
+				<div>
+        		<h3 class="change-detail-title ability-title"><img src="https://am-a.akamaihd.net/image?f=https://ddragon.leagueoflegends.com/cdn/15.9.1/img/item/3115.png">Nashor's Tooth</h3>
+					<ul>
+						<li><strong>Total Gold Cost</strong>: 3000 ⇒ <strong>2900</strong></li>
+					</ul>
+				</div>
+			`,
+    },
+    3089: {
+      change: CHANGE_TYPES.buff,
+      details: `
+				<div>
+        		<h3 class="change-detail-title ability-title"><img src="https://am-a.akamaihd.net/image?f=https://ddragon.leagueoflegends.com/cdn/15.9.1/img/item/3089.png">Rabadon's Deathcap</h3>
+					<ul>
+						<li><strong>Total Gold Cost</strong>: 3600 ⇒ <strong>3500</strong></li>
+					</ul>
+				</div>
+			`,
+    },
+    3040: {
       change: CHANGE_TYPES.nerf,
       details: `
-        <div>
-					<a href="https://www.leagueoflegends.com/en-us/how-to-play/" class="reference-link"><img src="https://am-a.akamaihd.net/image?f=https://ddragon.leagueoflegends.com/cdn/15.8.1/img/item/3010.png"></a>
-					<h3 id="patch-Symbiotic-Soles" class="change-title"><a href="https://www.leagueoflegends.com/en-us/how-to-play/">Symbiotic Soles</a></h3>
-					<blockquote class="blockquote context">
-						Symbiotic Soles are a solid option for supports, which we’re very happy about, but they’re too game-warping for laners, as the empowered Recall lets them quickly reset every few waves, removing all sense of attrition. So we’re making some adjustments to retain their power for roaming around the map but make them worse when spamming Recall over and over.
-						
-					</blockquote>
+				<div>
+        		<h3 class="change-detail-title ability-title"><img src="https://am-a.akamaihd.net/image?f=https://ddragon.leagueoflegends.com/cdn/15.9.1/img/item/3040.png">Seraph's Embrace</h3>
 					<ul>
-						<li><strong>NEW</strong>: Now grants 10 out of combat Movement Speed</li>
-						<li><strong>REMOVED</strong>: No longer grants Empowered Recall</li>
+						<li><strong>Lifeline Shield</strong>: 200 (+ 20% Current Mana) ⇒ <strong>18% Max Mana</strong></li>
 					</ul>
 				</div>
-      `,
+			`,
     },
-    6657: {
-      change: CHANGE_TYPES.nerf,
+    4646: {
+      change: CHANGE_TYPES.change,
       details: `
-        <div>
-					<a href="https://www.leagueoflegends.com/en-us/how-to-play/" class="reference-link"><img src="https://am-a.akamaihd.net/image?f=https://ddragon.leagueoflegends.com/cdn/15.8.1/img/item/6657.png"></a>
-					<h3 id="patch-Rod-of-Ages" class="change-title"><a href="https://www.leagueoflegends.com/en-us/how-to-play/">Rod of Ages</a></h3>
-					<blockquote class="blockquote context">
-						Rod of Ages’ new build path has made it overtake mage mana itemization, and as a result, its high durability has slowed down champion interactions below what we think makes for the best gameplay. We think as a whole, mages are rather balanced and we intend to ship a larger pass on mage itemization to make sure all AP items are worthy of purchasing in an upcoming patch, but right now we want to tap the newly-popular Rod of Ages to make sure bursty champions can attack more normal-sized health pools.
-						
-					</blockquote>
+				<div>
+        		<h3 class="change-detail-title ability-title"><img src="https://am-a.akamaihd.net/image?f=https://ddragon.leagueoflegends.com/cdn/15.9.1/img/item/4646.png">Stormsurge</h3>
 					<ul>
-						<li><strong>Initial Health</strong>: 400 ⇒ 350</li>
-						<li><strong>Maximum Health</strong>: 500 ⇒ 450</li>
-						<li><strong>Initial Ability Power</strong>: 50 ⇒ 45</li>
-						<li><strong>Maximum Ability Power</strong>: 80 ⇒ 75</li>
+						<li><strong>Total Gold Cost</strong>: 2900 ⇒ <strong>2800</strong></li>
+						<li><strong>Move Speed</strong>: 4% ⇒ <strong>6%</strong></li>
+						<li><strong>Gold on preemptive takedown</strong>: <strong>Removed</strong></li>
+						<li><strong>Move Speed on Squall</strong>: <strong>Removed</strong></li>
 					</ul>
 				</div>
-      `,
-    },
-    3174: {
-      change: CHANGE_TYPES.buff,
-      details: `
-        <div>
-					<h4 class="change-detail-title">Armored Advance</h4>
-					<ul>
-						<li><strong>Cost</strong>: 750 ⇒ 500 gold</li>
-					</ul>
-				</div>
-      `,
-    },
-    3173: {
-      change: CHANGE_TYPES.buff,
-      details: `
-        <div>
-					<h4 class="change-detail-title">Armored Advance</h4>
-					<ul>
-						<li><strong>Cost</strong>: 750 ⇒ 500 gold</li>
-					</ul>
-				</div>
-      `,
-    },
-    3171: {
-      change: CHANGE_TYPES.buff,
-      details: `
-        <div>
-					<h4 class="change-detail-title">Armored Advance</h4>
-					<ul>
-						<li><strong>Cost</strong>: 750 ⇒ 500 gold</li>
-					</ul>
-				</div>
-      `,
-    },
-    3176: {
-      change: CHANGE_TYPES.buff,
-      details: `
-        <div>
-					<h4 class="change-detail-title">Forever Forward</h4>
-					<ul>
-						<li><strong>Cost</strong>: 750 ⇒ 500 gold</li>
-						<li><strong>Total Bonus Movespeed Out of Combat</strong>: 10% ⇒ 8%</li>
-					</ul>
-				</div>
-      `,
-    },
-    3172: {
-      change: CHANGE_TYPES.buff,
-      details: `
-        <div>
-					<h4 class="change-detail-title">Armored Advance</h4>
-					<ul>
-						<li><strong>Cost</strong>: 750 ⇒ 500 gold</li>
-					</ul>
-				</div>
-      `,
-    },
-    3175: {
-      change: CHANGE_TYPES.buff,
-      details: `
-        <div>
-					<h4 class="change-detail-title">Spellslinger’s Shoes</h4>
-					<ul>
-						<li><strong>Cost</strong>: 750 ⇒ 500 gold</li>
-						<li><strong>Magic Penetration</strong>: 8% ⇒ 7%</li>
-					</ul>
-				</div>
-      `,
-    },
-    3170: {
-      change: CHANGE_TYPES.buff,
-      details: `
-        <div>
-					<h4 class="change-detail-title">Swiftmarch</h4>
-					<ul>
-						<li><strong>Cost</strong>: 750 ⇒ 500 gold</li>
-						<li><strong>Movement Speed</strong>: 70 ⇒ 65</li>
-						<li><strong>REMOVED</strong>: No longer grants 4% increased total Movement Speed</li>
-						<li><strong>NEW</strong>: Now grants Adaptive Force equal to 5% of total Movement Speed</li>
-					</ul>
-				</div>
-      `,
+			`,
     },
   },
 };
