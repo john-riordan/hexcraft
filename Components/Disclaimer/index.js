1a3420ca3a6cf75cc3e44fad72068796a26f634b
John Riordan <iam.john.riordan@gmail.com>
2025-12-09 00:38:21 -0800

    feat: design updates

--- Components/Disclaimer/index.js
+++ Components/Disclaimer/index.js
@@ -3,6 +3,13 @@ import styles from "./Disclaimer.module.css";
 const Disclaimer = () => {
   return (
     <header className={styles.disclaimer}>
+      <video
+        src="https://blitz-cdn-videos.blitz.gg/lol/misc/animated-demacia.webm"
+        autoPlay
+        muted
+        loop
+        playsInline
+      />
       <h1>League of Legends Items - Season 2026 (PBE)</h1>
       <span>
         Season 2026 item updates for League of Legends including all 9 new items
